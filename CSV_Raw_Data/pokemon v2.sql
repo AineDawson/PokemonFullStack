34e5ec030fb489ae94af99fe9ad4a7382b7a3f89
Dawson Murphy <demurphy2013@gmail.com>
2022-07-20 10:35:05 -0400

    New CVS
    Implementing 1st and 3rd normalization.

--- CSV_Raw_Data/pokemon v2.sql
+++ CSV_Raw_Data/pokemon v2.sql
@@ -2,13 +2,13 @@ CREATE TABLE Pokemon(
     dex_number Integer(4) not null,
     english_name varchar(30) not null,
     legend_status varchar(30),
-    catch_rate integer(10),
-    base_friendship int(9),
+    catch_rate Integer(10),
+    base_friendship Integer(9),
     growth_rate varchar(20),
     primary key(dex_number)
 );
 
-LOAD DATA LOCAL INFILE "C:\Users\demur\OneDrive\Documents\GitHub\PokemonFullStack\CSV_Raw_Data\pokémon.sql"
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/pokemon.csv"
 INTO TABLE Pokemon
 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
@@ -32,12 +32,23 @@ CREATE TABLE PokemonForm(
     FOREIGN KEY (dex_number) REFERENCES Pokemon(dex_number)
 );
 
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/PokemonForm.csv"
+INTO TABLE PokemonForm
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 CREATE TABLE Abilities(
     abilities_index Integer(9) not null,
     ability_name varchar(45),
-    ability_description varchar(45),
+    ability_description varchar(150),
+    generation integer(2),
     primary key(abilities_index)
 );
+
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/Abilities.csv"
+INTO TABLE Abilities
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 CREATE TABLE PokemonFormAbilities(
     dex_number Integer(9) not null,
@@ -48,11 +59,21 @@ CREATE TABLE PokemonFormAbilities(
     FOREIGN KEY (ability_index) REFERENCES Abilities(abilities_index)
 );
 
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/PokemonFormAbilities.csv"
+INTO TABLE pokemonformabilities
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 CREATE TABLE Types(
     type_index Integer(9) not null,
     type_name varchar(20) not null,
     primary key(type_index)
 );
+
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/Types.csv"
+INTO TABLE types
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 CREATE TABLE TypeMatchups(
     attack_type_index Integer(9) not null,
@@ -63,6 +84,11 @@ CREATE TABLE TypeMatchups(
     FOREIGN KEY (defense_type_index) REFERENCES Types(type_index)
 );
 
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/TypeMatchups.csv"
+INTO TABLE typematchups
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 CREATE TABLE PokemonFormTypes(
     dex_number Integer(9) not null,
     form_name varchar(45) not null,
@@ -71,6 +97,11 @@ CREATE TABLE PokemonFormTypes(
     FOREIGN KEY (dex_number, form_name) REFERENCES PokemonForm(dex_number, form_name),
     FOREIGN KEY (type_index) REFERENCES Types(type_index)
 );
+
+LOAD DATA LOCAL INFILE "C:/Users/demur/Documents/Coding/Practice Code/Full Stack Pokemon App/CSV_Raw_Data/PokemonFormTypes.csv"
+INTO TABLE pokemonformtypes
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 DROP TABLE PokemonFormTypes;
 DROP TABLE TypeMatchups;
